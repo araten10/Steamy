70e3fa032b3b85b55ade581d75ad01ad17bea64f
LewdDevelopment <158585445+LewdDevelopment@users.noreply.github.com>
2025-11-28 22:21:44 +0200

    Copy example image to grid

--- src/__main__.py
+++ src/__main__.py
@@ -1,22 +1,32 @@
-from tkinter import Tk
+import platform
+import shutil
+import tkinter as tk
+from pathlib import Path
 from tkinter import ttk
 
-from tkinter import (
-    GROOVE,
-    RAISED,
-    Button,
-    Checkbutton,
-    Frame,
-    Label,
-    OptionMenu,
-    Scale,
-    StringVar,
-    Text,
-)
+
+def pornify() -> None:
+    # TODO: There can be other paths, maybe multiple?
+    match platform.system():
+        case "Linux":
+            steam_path = Path("~/.local/share/Steam").expanduser()
+        case "Windows":
+            steam_path = Path()
+
+    # TODO: There can be multiple users
+    user_path = list((steam_path / "userdata").iterdir())[0]
+    grid_path = user_path / "config" / "grid"
+    grid_path.mkdir(parents=True, exist_ok=True)
+
+    example = Path(__file__).parent.parent / "example.png"
+    shutil.copyfile(example, grid_path / "4000_hero.png")
+
+    print("Done")
+
 
 if __name__ == "__main__":
-    root = Tk()
-    root.geometry('320x240')
-    
-    ttk.Button(root, text='Pornify').pack(pady=5, ipady=10, ipadx=5, expand=1)
+    root = tk.Tk()
+    root.geometry("320x240")
+
+    ttk.Button(root, text="Pornify", command=pornify).pack(pady=5, ipady=10, ipadx=5, expand=1)
     root.mainloop()
