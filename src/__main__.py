a08acb866e124761b0b69063d2b9412d5e54d0f1
araten10 <141581742+araten10@users.noreply.github.com>
2025-11-28 06:28:04 -0500

    very very initial gui (wip) (real) (beta 0.001)

--- src/__main__.py
+++ src/__main__.py
@@ -1,2 +1,22 @@
+from tkinter import Tk
+from tkinter import ttk
+
+from tkinter import (
+    GROOVE,
+    RAISED,
+    Button,
+    Checkbutton,
+    Frame,
+    Label,
+    OptionMenu,
+    Scale,
+    StringVar,
+    Text,
+)
+
 if __name__ == "__main__":
-    print("Hello World!")
+    root = Tk()
+    root.geometry('320x240')
+    
+    ttk.Button(root, text='Pornify').pack(pady=5, ipady=10, ipadx=5, expand=1)
+    root.mainloop()
