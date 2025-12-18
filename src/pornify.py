5986ad89b42a0c7edd28ed1af96fb32f5a28e1d5
araten10 <141581742+araten10@users.noreply.github.com>
2025-12-18 05:46:30 -0500

    remove attempted refresh code

--- src/pornify.py
+++ src/pornify.py
@@ -2,8 +2,6 @@ import asyncio
 import logging
 import random
 import shutil
-# import os
-# import time
 from json.decoder import JSONDecodeError
 
 import aiohttp
@@ -167,8 +165,6 @@ class PornifyThread(QThread):
                         async with session.get(url) as image_res:
                             with open(self.grid.path / f"{game.id}{art.suffix}.png", "wb") as f:
                                 f.write(await image_res.read())
-                            # curr_time = time.time()
-                            # os.utime(self.grid.path / f"{game.id}{art.suffix}.png", (curr_time, curr_time))
                             break
                 except ClientError as e:
                     logging.warning(f"Image download failed with {type(e).__name__}: {e}, retrying...")
